33bc6097892d8c49923ef8d9959b014a559f3fd1
Roxanoel <roxane.noel92@gmail.com>
2022-07-18 14:04:43 +0100

    Improved error handling. Messaging changes depending on whether the failure has to do with a formatting issue or the API failing to find the location.

--- src/index.js
+++ src/index.js
@@ -5,31 +5,33 @@ import { parseSearchInput } from "./ui-search-util";
 // #region CACHED REFS
 const form = document.getElementById('location-form');
 const searchInput = document.getElementById('search-input');
-const errorMsgSpan = document.getElementById('search-error');
+const errorMsg = document.getElementById('search-error');
 // #endregion
 
 // #region ADDING LISTENERS
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    errorMsgSpan.textContent = '';
+    errorMsg.textContent = '';
     // Validation
     if (searchInput.validity.valid) {
         // If correct, handle data for API call
         const parsedInput = parseSearchInput(searchInput.value);
         geocodingLocation(parsedInput[0], parsedInput[1], parsedInput[2])
         .then(locationData => getCurrentWeatherData(locationData))
-        .then(weatherData => console.log(weatherData));
+        .then(weatherData => console.log(weatherData))
+        .catch(() => displayInputError('Location not found. '));
     }
     // If incorrect, display error message detailing format. 
     else {
-        displayInputError();
+        displayInputError('Invalid format. ');
     }
 })
 // #endregion
 
 // #region FUNCTION DEFS
-    function displayInputError() {
-        errorMsgSpan.textContent = 'Please use the following format: "City", "City, Country", or (for USA only) "City, State, Country"';
+    function displayInputError(preface) {
+        errorMsg.textContent = `${preface}Please use the following format: 
+        "City", "City, Country", or (for USA only) "City, State, Country".`;
     }
 // #endregion 
 
