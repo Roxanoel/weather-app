da9fb9f8b54ff616d6939b48a176fbc2f7ad9491
Roxanoel <roxane.noel92@gmail.com>
2022-07-18 12:01:42 +0100

    Error message displays when input field invalid.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+/* eslint-disable no-use-before-define */
 import { geocodingLocation, getCurrentWeatherData } from "./weather-data";
 import { parseSearchInput } from "./ui-search-util";
 
 // #region CACHED REFS
 const form = document.getElementById('location-form');
 const searchInput = document.getElementById('search-input');
+const errorMsgSpan = document.getElementById('search-error');
 // #endregion
 
 // #region ADDING LISTENERS
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    errorMsgSpan.textContent = '';
     // Validation
     if (searchInput.validity.valid) {
         // If correct, handle data for API call
@@ -18,8 +21,17 @@ form.addEventListener('submit', (e) => {
         .then(weatherData => console.log(weatherData));
     }
     // If incorrect, display error message detailing format. 
+    else {
+        displayInputError();
+    }
 })
 // #endregion
+
+// #region FUNCTION DEFS
+    function displayInputError() {
+        errorMsgSpan.textContent = 'Please use the following format: "City", "City, Country", or (for USA only) "City, State, Country"';
+    }
+// #endregion 
 
 
 geocodingLocation('London', '', '')
