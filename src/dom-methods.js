6fe0063d6401645ec2d237ea096707e1623fc959
Roxanoel <roxane.noel92@gmail.com>
2022-07-20 17:18:18 +0100

    Weather icon updates after submitting search query

--- src/dom-methods.js
+++ src/dom-methods.js
@@ -9,9 +9,23 @@ function updateTemperature(temp, tempUnits) {
     tempText.innerHTML = `${Math.round(temp)}${unitSymbol}`;
 }
 
+function updateIcon(id) {
+    const iconClassCode = `wi-owm-${id}`;
+    const icon = document.querySelector('.hero i');
+    // first, clear class list: 
+    const iconClasses = icon.classList;
+    while (iconClasses.length > 0) {
+    iconClasses.remove(iconClasses.item(0));
+    }
+    // re-add the right classes
+    iconClasses.add('wi');
+    iconClasses.add(iconClassCode);
+}
+
 function updateUi(weatherData, tempUnits) {
     updateLocationName(weatherData.location);
     updateTemperature(weatherData.temp, tempUnits);
+    updateIcon(weatherData.id);
 }
 
 export {
